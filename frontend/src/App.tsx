026f08743181144f953995e33db533fc4dc02146
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-10-07 17:30:59 -0700

    chore: separate frontend and backend code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,6 @@ import { Button } from "@/components/ui/button";
 export default function App() {
   return <div>
       <Button>hello world</Button>
-      <img src="../public/campus_map.jpeg" alt="campus map" width={600} height={300}/>
+      <img src="/campus_map.jpeg" alt="campus map" width={600} height={300}/>
   </div>
 }
